7ab71255bbc3842d7218a0762a3b30869888aaa2
iinuXD <kittiphat.ra@kkumail.com>
2026-02-08 20:13:19 +0700

    fixed lab06

--- apps/lab06-expense-vue/vite.config.ts
+++ apps/lab06-expense-vue/vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lab06-expense-vue/',
+  base: '/mobileweb/lab06-expense-vue/',
   plugins: [
     vue(),
     legacy()
