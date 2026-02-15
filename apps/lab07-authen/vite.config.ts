ace7fd48751a9ebaf86bc8a1cff89a131e236792
iinuXD <kittiphat.ra@kkumail.com>
2026-02-15 21:09:47 +0700

    fix 2 lb07

--- apps/lab07-authen/vite.config.ts
+++ apps/lab07-authen/vite.config.ts
@@ -7,11 +7,12 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mobileweb/lab07-authen/',
   plugins: [
     react(),
     legacy()
   ],build: {
-    outDir: path.resolve(__dirname, '../../docs/lab07-auten'),
+    outDir: path.resolve(__dirname, '../../docs/lab07-authen'),
     emptyOutDir: true
   },
   test: {
