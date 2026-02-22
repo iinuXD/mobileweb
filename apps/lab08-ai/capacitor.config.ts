3dc7dfaaeb47dc4cdf221a74ea2d2813f0f23218
iinuXD <kittiphat.ra@kkumail.com>
2026-02-22 17:13:52 +0700

    Revert "lab08"
    This reverts commit 224c0ab3fb4b7b8d1238163a0e0aced0f02781eb.

--- apps/lab08-ai/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'cpkku.kittiphat.lab08',
-  appName: 'lab07-authen',
-  webDir: 'dist'
-};
-
-export default config;
